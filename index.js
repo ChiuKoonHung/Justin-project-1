ec31d3a98ac64e95cc24fbc9f6cf6fd34a39fd2b
ChiuKoonHung <chiukoonhung1991@gmail.com>
2021-09-02 01:02:31 +0800

    draft

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 
+window.addEventListener("submit", selection);
+
+
+function selection(evt) {
+    evt.preventDefault();
+    
+ 
+    const choiceCarbs = document.getElementById("carbs").value;
+    const choiceIngredient = document.getElementById("ingredient").value;
+
+
+    if (choiceCarbs == 0 && choiceIngredient == 0) {
+        return alert("Please choose carb & ingredient.");
+    }
+    else if (choiceCarbs == 0) {
+        return alert("Please choose a carb.");
+    }
+    else if (choiceIngredient == 0) {
+        return alert("Please choose an ingredient.");
+    }
+    else {
+        return alert(`Your carbs: ${choiceCarbs}\nYour ingredient: ${choiceIngredient}`); 
+    }
+    
+}
+
+// class Cook {
+//     cookTheRice() {
+//         document.getElementById("printCook").innerHTML = "Put in cooker for about 40 minutes.";
+        
+//     }
+//     final() {
+//         this.cookTheRice();
+//     }
+// }
+// const cook1 = new Cook();
+// if (this.choiceCarbs === "brownRice") {
+//     cook1.final();
+// }
+
+
+
+// const selection = {
+//     choiceCarbs: document.getElementById("carbs"),
+//     choiceIngredient: document.getElementById("ingredient")
+// };
