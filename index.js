e8a83a76cf61421a2e679a5a0054c4340027150c
ChiuKoonHung <chiukoonhung1991@gmail.com>
2021-09-02 18:19:00 +0800

    draft

--- index.js
+++ index.js
@@ -1,47 +1,81 @@
 
 window.addEventListener("submit", selection);
-
-
-function selection(evt) {
+function selection(evt) {  // function to check for input
     evt.preventDefault();
     
- 
     const choiceCarbs = document.getElementById("carbs").value;
-    const choiceIngredient = document.getElementById("ingredient").value;
+    const choiceProtein = document.getElementById("protein").value;
 
-
-    if (choiceCarbs == 0 && choiceIngredient == 0) {
-        return alert("Please choose carb & ingredient.");
+    if (choiceCarbs == 0 && choiceProtein == 0) {
+        return alert("Please choose carb & protein.");
     }
     else if (choiceCarbs == 0) {
         return alert("Please choose a carb.");
     }
-    else if (choiceIngredient == 0) {
-        return alert("Please choose an ingredient.");
+    else if (choiceProtein == 0) {
+        return alert("Please choose an protein.");
     }
     else {
-        return alert(`Your carbs: ${choiceCarbs}\nYour ingredient: ${choiceIngredient}`); 
+        return alert(`Your carbs: ${choiceCarbs}\nYour protein: ${choiceProtein}`); 
     }
     
 }
 
-// class Cook {
-//     cookTheRice() {
-//         document.getElementById("printCook").innerHTML = "Put in cooker for about 40 minutes.";
-        
+window.addEventListener("submit", listOfInstructions);
+function listOfInstructions(evt) {    // function to print out instructions after checking input
+    evt.preventDefault();
+    
+    const choiceCarbs = document.getElementById("carbs").value;
+    const choiceProtein = document.getElementById("protein").value;
+    const stepsForRiceChicken = ["Put rice in cooker for about 30 minutes.", "Marinate chicken breast with salt & pepper.", "Pan sear over medium heat for 5 mins on each side."];
+    const stepsForRiceEgg = ["Put rice in cooker for about 30 minutes.", ""];
+
+    if (choiceCarbs == "brownrice" && choiceProtein == "chicken") {
+        for (let i = 0; i < stepsForRiceChicken.length; i++) {
+            const textNode = document.createTextNode(stepsForRiceChicken[i]);
+            const liNode = document.createElement("li");
+            liNode.appendChild(textNode);
+            document.getElementById("list").appendChild(liNode);      
+        }
+        return true;
+    }
+    else if (choiceCarbs == "brownrice" && choiceProtein == "egg") {
+        for (let i = 0; i < stepsForRiceEgg.length; i++) {
+            const textNode = document.createTextNode(stepsForRiceEgg[i]);
+            const liNode = document.createElement("li");
+            liNode.appendChild(textNode);
+            document.getElementById("list").appendChild(liNode);      
+        }
+        return true;
+    }
+}
+
+window.addEventListener("submit", header);
+function header(evt) {      // function to print header
+    evt.preventDefault();
+    
+    const choiceCarbs = document.getElementById("carbs").value;
+    const choiceProtein = document.getElementById("protein").value;
+
+    if (choiceCarbs !== 0 && choiceProtein !== 0) {
+        return document.getElementById("CookingInstructions").innerHTML = "Cooking Instructions";
+    }
+}
+
+// function haha(evt) {
+//     evt.preventDefault();
+
+//     class Cook {
+//         constructor (choiceCarbs, choiceIngredient) {
+//             choiceCarbs = document.getElementById("carbs").value,
+//             choiceProtein = document.getElementById("protein").value
+//         };
+//         send() {
+//         if (this.choiceCarbs == "brownrice") {
+//         return document.getElementById("printCook").innerHTML = "Put in cooker for about 40 minutes.";
+//         }
+//         }
 //     }
-//     final() {
-//         this.cookTheRice();
-//     }
+//     const hehe = new Cook();
+//     hehe.send();
 // }
-// const cook1 = new Cook();
-// if (this.choiceCarbs === "brownRice") {
-//     cook1.final();
-// }
-
-
-
-// const selection = {
-//     choiceCarbs: document.getElementById("carbs"),
-//     choiceIngredient: document.getElementById("ingredient")
-// };
